355dd7f73f657bdcbddf04b935c2124852267cce
gyuha <nicegyuha@gmail.com>
2019-03-19 12:27:44 +0900

    update

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -11,3 +11,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A1
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt update
 sudo apt install -y mongodb-org
+sudo sed -i "s/bindIp: 127.0.0.1/bindIp: 0.0.0.0/" /etc/mongod.conf
+sudo systemctl enable mongod
+sudo service mongod restart
---
+++ vcli-state.bat
@@ -0,0 +1,2 @@
+vagrant global-status
+pause
